a471902296529e4e67127f0766adc89799e4a771
Maxence Vérité <p.verite.maxence@gmail.com>
2021-05-09 21:47:27 +0200

    colors and responsive + books

---
+++ js/mailSending.js
@@ -0,0 +1,6 @@
+function sendMail() {
+  let name = document.getElementById("name").value;
+  let mailFrom = document.getElementById("email").value;
+  let phoneFrom = document.getElementById("phone").value;
+  let message = document.getElementById("message").value;
+}
